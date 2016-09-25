4387132149e09c096cf6cda450fd9e6d7f308452
赤燭包子 <huw12313212@gmail.com>
2016-09-26 01:58:56 +0800

    +TurretManagerScript

---
+++ Assets/TurretManagerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretManagerScript : MonoBehaviour {
+
+	private Animator _animator;
+
+	void Start () {
+		_animator = this.GetComponent<Animator> ();
+	}
+
+	private void PlayShootAnimation(){
+		_animator.SetTrigger ("Shoot");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			PlayShootAnimation ();
+		}
+	}
+}
