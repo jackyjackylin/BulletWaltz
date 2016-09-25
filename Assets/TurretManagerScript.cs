ec74fe998d82cbe6339f757925e71819ef73d219
赤燭包子 <huw12313212@gmail.com>
2016-09-26 02:46:43 +0800

    +大砲Rotate動畫

--- Assets/TurretManagerScript.cs
+++ Assets/TurretManagerScript.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class TurretManagerScript : MonoBehaviour {
 
 	private Animator _animator;
+
+	const int DirectionCount = 8;
+	public Ease RotateEaseFunction;
+	public float rotateDuration;
+
 
 	void Start () {
 		_animator = this.GetComponent<Animator> ();
@@ -12,11 +18,20 @@ public class TurretManagerScript : MonoBehaviour {
 	private void PlayShootAnimation(){
 		_animator.SetTrigger ("Shoot");
 	}
+
+	private void PlayRotateAnimation(){
+			float targetDegree = 360.0f / DirectionCount * Random.Range (0, DirectionCount);
+			this.transform.DORotate (new Vector3 (0, 0, targetDegree), rotateDuration);
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			PlayShootAnimation ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+			PlayRotateAnimation ();
 		}
 	}
 }
