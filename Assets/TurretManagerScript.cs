ddc2e34ce4e38e36127d9b0ff27b6c30431516e1
赤燭包子 <huw12313212@gmail.com>
2016-10-02 03:01:08 +0800

    +Follow the beat

--- Assets/TurretManagerScript.cs
+++ Assets/TurretManagerScript.cs
@@ -19,12 +19,12 @@ public class TurretManagerScript : MonoBehaviour {
 		_animator = this.GetComponent<Animator> ();
 	}
 
-	private void PlayShootAnimation(){
+	public void PlayShootAnimation(){
 		_animator.SetTrigger ("Shoot");
 		GameCamera.transform.DOShakePosition (CameraShakeDuration, CameraShakeStrenth);
 	}
 
-	private void PlayRotateAnimation(){
+	public void PlayRotateAnimation(){
 			float targetDegree = 360.0f / DirectionCount * Random.Range (0, DirectionCount);
 			this.transform.DORotate (new Vector3 (0, 0, targetDegree), rotateDuration);
 	}
